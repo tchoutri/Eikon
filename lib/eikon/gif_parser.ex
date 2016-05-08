7ed3b84969cc1bd5da7e2174e80fa2a26d837924
Théophile Choutri <theophile@choutri.eu>
2016-05-09 00:45:35 +0200

    unused variables

--- lib/eikon/gif_parser.ex
+++ lib/eikon/gif_parser.ex
@@ -12,19 +12,20 @@ defmodule Eikon.GIF.Parser do
   """
   alias Eikon.GIF
 
-  @gif struct
+  @type gif :: struct()
+
   @magic89 <<0x47, 0x49, 0x46, 0x38, 0x39, 0x61>>
   @magic87 <<0x47, 0x49, 0x46, 0x38, 0x37, 0x61>>
 
-  def magic?(<<@magic89, rest :: binary>>), do: true
-  def magic?(<<@magic87, rest :: binary>>), do: true
+  def magic?(<<@magic89, _rest :: binary>>), do: true
+  def magic?(<<@magic87, _rest :: binary>>), do: true
   def magic?(_),                            do: false
 
-  @spec infos(bistring) :: gif
-  def infos(<<@magic89, width :: size(16), height :: size(16), rest :: binary>>) do
+  @spec infos(bitstring) :: gif
+  def infos(<<@magic89, width :: size(16), height :: size(16), _rest :: binary>>) do
     %GIF{width: width, height: height, version: "89a"}
   end
-  def infos(<<@magic87, width :: size(16), height :: size(16), rest :: binary>>) do
+  def infos(<<@magic87, width :: size(16), height :: size(16), _rest :: binary>>) do
     %GIF{width: width, height: height, version: "89a"}
   end
 end
